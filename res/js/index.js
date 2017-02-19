9fcd49fe2f41b808887068f61e1d4a3db5a86229
Connor de la Cruz <connor.c.delacruz@gmail.com>
2017-02-19 15:22:53 -0600

    styling and ux fixes.
    - Added transition to resume download button
    - Changed how affix offset of navbar is calculated

--- res/js/index.js
+++ res/js/index.js
@@ -2,16 +2,16 @@
 
 
 $(function () {
-    // set navbar affix offset to the height of the page header. Since we pass it a function, it's dynamically calculated
-    $('#navbar').affix({
+    var navbar = $('#navbar');
+    navbar.affix({
         offset: {
-            top: function() {return $('#page-header').outerHeight();}
+            top: navbar.offset().top
         }
     });
 
     // for smooth scrolling links
     // navbar
-    $("#navbar a").on('click', function(event) {
+    navbar.find('a').on('click', function(event) {
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
             // Prevent default anchor click behavior
@@ -24,7 +24,7 @@ $(function () {
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 800, function(){
+            }, 800, function() {
 
                 // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
