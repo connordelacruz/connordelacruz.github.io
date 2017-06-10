9470f266ac8da2049b62defd5e1d82c0dc6e9089
connordelacruz <connor.c.delacruz@gmail.com>
2017-06-10 18:59:10 -0500

    Removed trivial comments

--- res/js/index.js
+++ res/js/index.js
@@ -14,14 +14,11 @@ $(function () {
     navbar.find('a').click(function(event) {
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== "") {
-            // Prevent default anchor click behavior
             event.preventDefault();
 
-            // Store hash
             var hash = this.hash;
 
             // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 // Subtract the size of navbar container (if not affixed will compensate for increased page height, if affixed compensates for navbar height)
                 scrollTop: $(hash).offset().top - $('#navbar-container').outerHeight()
