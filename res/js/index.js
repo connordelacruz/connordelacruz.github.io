efd50334395c3bdb4f7ff8576159b642b5374969
Connor de la Cruz <connor.c.delacruz@gmail.com>
2019-11-15 11:25:39 -0600

    Fixed contact icon pop in

--- res/js/index.js
+++ res/js/index.js
@@ -9,87 +9,89 @@
  * @constructor
  */
 function ContactLinkObject(name, url, svg) {
-    this.name = name;
-    this.url = url;
-    this.svg = 'res/svg/' + svg;
+  this.name = name;
+  this.url = url;
+  this.svg = 'res/svg/' + svg;
 }
 
 /* Variables */
 
 var contactLinks = [
-    new ContactLinkObject('GitHub', 'https://github.com/connordelacruz', 'github.svg'),
-    new ContactLinkObject('LinkedIn', 'http://www.linkedin.com/in/connordelacruz', 'linked.svg'),
-    new ContactLinkObject('Email', 'mailto:connor.c.delacruz@gmail.com', 'email.svg'),
-    new ContactLinkObject('Instagram', 'https://www.instagram.com/delachrome', 'gram.svg')
+  new ContactLinkObject('GitHub', 'https://github.com/connordelacruz', 'github.svg'),
+  new ContactLinkObject('LinkedIn', 'http://www.linkedin.com/in/connordelacruz', 'linked.svg'),
+  new ContactLinkObject('Email', 'mailto:connor.c.delacruz@gmail.com', 'email.svg'),
+  new ContactLinkObject('Instagram', 'https://www.instagram.com/delachrome', 'gram.svg')
 ];
 
 /* Functions */
 
 function buildContactLinksContainer(contactLinks) {
-    var contactLinksContainer = '<div class="btn-group">';
-    $.each(contactLinks, function (i, contactLink) {
-        contactLinksContainer += buildContactLink(contactLink);
-    });
-    contactLinksContainer += '</div>';
-    return contactLinksContainer;
+  var contactLinksContainer = '<div class="btn-group">';
+  $.each(contactLinks, function (i, contactLink) {
+    contactLinksContainer += buildContactLink(contactLink);
+  });
+  contactLinksContainer += '</div>';
+  return contactLinksContainer;
 }
 
 function buildContactLink(contactLink) {
-    return '<a class="btn btn-link" href="' + contactLink.url + '" target="_blank">' +
-        '<img class="svg" src="' + contactLink.svg + '"/>' +
-        '</a>';
+  return '<a class="btn btn-link" href="' + contactLink.url + '" target="_blank">' +
+    '<img class="svg" src="' + contactLink.svg + '"/>' +
+    '</a>';
 }
 
 function showCopyright() {
-    $('.copyright').html('&copy; ' + new Date().getFullYear());
+  $('.copyright').html('&copy; ' + new Date().getFullYear());
 }
 
 
 $(function () {
-    // Populate contact links
-    var contactLinksMarkup = buildContactLinksContainer(contactLinks);
-    $('.contact-icons').html(contactLinksMarkup).addClass('in');
+  // Populate contact links
+  var contactLinksMarkup = buildContactLinksContainer(contactLinks);
+  var contactIconsContainers = $('.contact-icons');
+  contactIconsContainers.html(contactLinksMarkup);
+  contactIconsContainers.addClass('in');
 
-    var navbar = $('#navbar');
-    // Enable affixed navbar
-    navbar.affix({
-        offset: {
-            top: navbar.offset().top
-        }
-    });
-    // Enable scrollspy
-    $('body').scrollspy({
-        target: '.navbar',
-        offset: 70
-    });
+  var navbar = $('#navbar');
+  // Enable affixed navbar
+  navbar.affix({
+    offset: {
+      top: navbar.offset().top
+    }
+  });
+  // Enable scrollspy
+  $('body').scrollspy({
+    target: '.navbar',
+    offset: 70
+  });
 
-    // for smooth scrolling links
-    navbar.find('a').click(function(event) {
-        // Make sure this.hash has a value before overriding default behavior
-        if (this.hash !== "") {
-            event.preventDefault();
+  // for smooth scrolling links
+  navbar.find('a').click(function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      event.preventDefault();
 
-            var hash = this.hash;
+      var hash = this.hash;
 
-            // Using jQuery's animate() method to add smooth page scroll
-            $('html, body').animate({
-                // Subtract the size of navbar container (if not affixed will compensate for increased page height, if affixed compensates for navbar height)
-                scrollTop: $(hash).offset().top - $('#navbar-container').outerHeight()
-            }, 400);
-        }
-    });
+      // Using jQuery's animate() method to add smooth page scroll
+      $('html, body').animate({
+        // Subtract the size of navbar container (if not affixed will compensate for increased page height, if affixed compensates for navbar height)
+        scrollTop: $(hash).offset().top - $('#navbar-container').outerHeight()
+      }, 400);
+    }
+  });
 
-    // Collapse navbar w/ animation when body is clicked
-    $(document).click(function (event) {
-        var clickTarget = $(event.target);
-        var isOpened = $('.navbar-collapse').hasClass('collapse in');
-        // Collapse if navbar is open and this isn't the navbar-toggle button
-        if(isOpened && !clickTarget.hasClass('navbar-toggle')) {
-            $('button.navbar-toggle').click();
-        }
-    });
+  // Collapse navbar w/ animation when body is clicked
+  $(document).click(function (event) {
+    var clickTarget = $(event.target);
+    var isOpened = $('.navbar-collapse').hasClass('collapse in');
+    // Collapse if navbar is open and this isn't the navbar-toggle button
+    if(isOpened && !clickTarget.hasClass('navbar-toggle')) {
+      $('button.navbar-toggle').click();
+    }
+  });
 
-    $('#about-photo').on('dragstart', function(event) { event.preventDefault(); });
+  $('#about-photo').on('dragstart', function(event) { event.preventDefault(); });
 
-    showCopyright();
+  showCopyright();
 });
