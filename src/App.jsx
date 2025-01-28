a02ca629290e6fd5d8ee96aa583f06760b9f4383
Connor de la Cruz <connor.c.delacruz@gmail.com>
2025-01-27 18:19:47 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Container, Typography } from '@mui/material'
+
+function App() {
+
+  return (
+    <Container maxWidth="md">
+      <Typography variant="h1">hello world.</Typography>
+    </Container>
+  )
+}
+
+export default App
