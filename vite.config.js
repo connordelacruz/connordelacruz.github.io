827f10e8d618b297ea9c755f516fd0d63e0bbd91
Connor de la Cruz <connor.c.delacruz@gmail.com>
2025-02-18 17:37:55 -0600

    fixed relative paths on local vs gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: ".",
-  plugins: [react()],
-  server: {
-    open: true,
-  },
+export default defineConfig(({command, mode}) => {
+  return {
+    base: mode === 'production' ? "/connordelacruz-react" : "./",
+    plugins: [react()],
+    server: {
+      open: true,
+    },
+  }
 })
