14d46c193ac64d7ad306610b4e9a7bc548e063d4
Connor de la Cruz <connor.c.delacruz@gmail.com>
2025-04-03 11:20:01 -0500

    Extracted Projects section to file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { Box, Button, Container, CssBaseline, Paper, Stack, ThemeProvider, Typography } from '@mui/material'
-import { GitHub } from '@mui/icons-material'
+import { Container, CssBaseline, Paper, Stack, ThemeProvider, Typography } from '@mui/material'
 import { theme } from './components/Theme.jsx'
 import { ResumeSection } from './components/ResumeSection.jsx'
-import { ProjectCard } from './components/ProjectCard.jsx'
-import { SectionHeader } from './components/SectionHeader.jsx'
+import { ProjectsSection } from './components/ProjectsSection.jsx'
 import { NavBar } from './components/NavBar.jsx'
 import { PageHeader } from './components/PageHeader.jsx'
 import { ContactSection } from './components/ContactSection.jsx'
@@ -47,9 +45,7 @@ function App() {
       />
 
       { /* Header */}
-      <PageHeader
-        color={COLOR_HEADER}
-      />
+      <PageHeader color={COLOR_HEADER}/>
 
       {/* Page Content */}
       <Container id="page-wrapper" maxWidth="md">
@@ -58,154 +54,10 @@ function App() {
           spacing={4}
         >
           {/* Resume */}
-          <ResumeSection
-            color={COLOR_RESUME}
-          />
+          <ResumeSection color={COLOR_RESUME}/>
 
           {/* Projects */}
-          <Box id="section-projects">
-            <SectionHeader color={COLOR_PROJECTS}>Projects</SectionHeader>
-
-            <Stack
-              id="projects-stack"
-              spacing={2}
-            >
-              <ProjectCard
-                title="Channel ⇄ Shift"
-                imgSrc="images/projects/channel-shift.png"
-                bigButtonContent={{
-                  text: 'Visit Channel Shift Site',
-                  link: 'https://cheezwhiz.biz'
-                }}
-              >
-                A web app for creating "glitch art" by shifting and swapping an image's RGB color channels, developed as
-                a
-                web app with an intuitive UI to make it easily accessible for anyone to use. Built with React and P5.js.
-                Click the button below to try it out!
-              </ProjectCard>
-              <ProjectCard
-                title="React Counter App"
-                imgSrc="images/projects/react-counter.png"
-                bigButtonContent={{
-                  text: 'Visit React Counter Site',
-                  link: 'https://connordelacruz.com/react-counter/'
-                }}
-              >
-                A simple web app for creating customizable counters. Each counter can have a custom name, color,
-                increment/decrement value, and reset value. Counters can easily be reordered by clicking and dragging
-                them into place. Data is saved locally, so your counters will persist any time you come back to the
-                page. Created using React and MUI’s Material UI library.
-              </ProjectCard>
-              <ProjectCard
-                title="Channel ⇄ Shift Classic"
-                imgSrc="images/projects/channel-shift-classic.png"
-                smallButtonsContent={[
-                  {
-                    text: 'GitHub',
-                    link: 'https://github.com/connordelacruz/ChannelShiftGUI'
-                  }
-                ]}
-              >
-                {/*TODO: explain project being no longer maintained but also why it's worth using still?*/}
-                An older version of the Channel Shift tool created with Processing. Includes some experimental features
-                that haven't been ported to the web app yet.
-              </ProjectCard>
-              <ProjectCard
-                title="Chicago ASCII Art"
-                imgSrc="images/projects/chicago-ascii-art.png"
-                smallButtonsContent={[
-                  {
-                    text: 'GitHub',
-                    link: 'https://github.com/connordelacruz/chicago-ascii.sh'
-                  }
-                ]}
-              >
-                Bash scripts for displaying color ASCII art of Chicago flag/skyline in the terminal.
-              </ProjectCard>
-              <ProjectCard
-                title="iTerm2 Tab Color Commands"
-                imgSrc="images/projects/iterm2-tab-color.png"
-                smallButtonsContent={[
-                  {
-                    text: 'GitHub',
-                    link: 'https://github.com/connordelacruz/iterm2-tab-color'
-                  }
-                ]}
-              >
-                Bash commands for setting the iTerm2 tab/title bar colors. Pairs well with base16 shell themes.
-              </ProjectCard>
-              <ProjectCard
-                title="Git Workflow Tools"
-                smallButtonsContent={[
-                  {
-                    text: 'GitHub',
-                    link: 'https://github.com/connordelacruz/git-workflow'
-                  },
-                  {
-                    text: 'PyPI',
-                    link: 'https://pypi.org/project/git-workflow/'
-                  }
-                ]}
-              >
-                Command line tools to streamline our team's Git workflow. Written in Python.
-              </ProjectCard>
-              <ProjectCard
-                title="Python Random User Generator"
-                smallButtonsContent={[
-                  {
-                    text: 'GitHub',
-                    link: 'https://github.com/connordelacruz/python-randomuser'
-                  },
-                  {
-                    text: 'PyPI',
-                    link: 'https://pypi.org/pypi/randomuser'
-                  },
-                  {
-                    text: 'Docs',
-                    link: 'http://connordelacruz.com/python-randomuser/'
-                  }
-                ]}
-              >
-                A Python class for generating random user information using the randomuser.me API.
-              </ProjectCard>
-              <ProjectCard
-                title="Python Lorem Ipsum Generator"
-                smallButtonsContent={[
-                  {
-                    text: 'GitHub',
-                    link: 'https://github.com/connordelacruz/py-loremipsum'
-                  },
-                  {
-                    text: 'PyPI',
-                    link: 'https://pypi.org/pypi/py-loremipsum'
-                  },
-                  {
-                    text: 'Docs',
-                    link: 'http://connordelacruz.com/py-loremipsum/'
-                  }
-                ]}
-              >
-                A Python module for generating placeholder text using the loripsum.net API. Includes a command line tool
-                to generate text from the terminal and copy it to the clipboard.
-              </ProjectCard>
-
-              {/*View more button*/}
-              <Box id="github-link">
-                {/*TODO: big juicy CTA buttons*/}
-                <Button
-                  variant="contained"
-                  color={COLOR_PROJECTS}
-                  startIcon={<GitHub/>}
-                  size="large"
-                  fullWidth
-                  href="https://github.com/connordelacruz"
-                  target="_blank"
-                >
-                  View More on GitHub
-                </Button>
-              </Box>
-            </Stack>
-          </Box>
+          <ProjectsSection color={COLOR_PROJECTS}/>
 
           {/*Contact*/}
           <ContactSection color={COLOR_CONTACT}/>
---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,263 @@
+import { Box, Button, Card, CardActions, CardContent, CardHeader, Stack, Typography } from '@mui/material'
+import { SectionHeader } from './SectionHeader.jsx'
+import { GitHub } from '@mui/icons-material'
+
+// ================================================================================
+// Content
+// ================================================================================
+// TODO: spot check content
+const projects = [
+  {
+    title: "Channel ⇄ Shift",
+    imgSrc: "images/projects/channel-shift.png",
+    bigButtonContent: {
+      text: 'Visit Channel Shift Site',
+      link: 'https://cheezwhiz.biz',
+    },
+    body: 'A web app for creating "glitch art" by shifting and swapping an image\'s RGB color channels, developed as a web app with an intuitive UI to make it easily accessible for anyone to use. Built with React and P5.js. Click the button below to try it out!',
+  },
+  {
+    title: "React Counter App",
+    imgSrc: "images/projects/react-counter.png",
+    bigButtonContent: {
+      text: 'Visit React Counter Site',
+      link: 'https://connordelacruz.com/react-counter/'
+    },
+    body: 'A simple web app for creating customizable counters. Each counter can have a custom name, color, increment/decrement value, and reset value. Counters can easily be reordered by clicking and dragging them into place. Data is saved locally, so your counters will persist any time you come back to the page. Created using React and the Material UI library.',
+  },
+  {
+    title: "Channel ⇄ Shift Classic",
+    imgSrc: "images/projects/channel-shift-classic.png",
+    smallButtonsContent: [
+      {
+        text: 'GitHub',
+        link: 'https://github.com/connordelacruz/ChannelShiftGUI'
+      }
+    ],
+    body: 'An older version of the Channel Shift tool created with Processing. Includes some experimental features that haven\'t been ported to the web app yet.',
+  },
+  {
+    title: "Chicago ASCII Art",
+    imgSrc: "images/projects/chicago-ascii-art.png",
+    smallButtonsContent: [
+      {
+        text: 'GitHub',
+        link: 'https://github.com/connordelacruz/chicago-ascii.sh'
+      }
+    ],
+    body: 'Bash scripts for displaying color ASCII art of Chicago flag/skyline in the terminal.',
+  },
+  {
+    title: "iTerm2 Tab Color Commands",
+    imgSrc: "images/projects/iterm2-tab-color.png",
+    smallButtonsContent: [
+      {
+        text: 'GitHub',
+        link: 'https://github.com/connordelacruz/iterm2-tab-color'
+      }
+    ],
+    body: 'Bash commands for setting the iTerm2 tab/title bar colors. Integrates with base16 shell themes.',
+  },
+  {
+    title: "Git Workflow Tools",
+    smallButtonsContent: [
+      {
+        text: 'GitHub',
+        link: 'https://github.com/connordelacruz/git-workflow'
+      },
+      {
+        text: 'PyPI',
+        link: 'https://pypi.org/project/git-workflow/'
+      }
+    ],
+    body: 'Command line tools to streamline our team\'s Git workflow. Written in Python.',
+  },
+  {
+    title: "Python Random User Generator",
+    smallButtonsContent: [
+      {
+        text: 'GitHub',
+        link: 'https://github.com/connordelacruz/python-randomuser'
+      },
+      {
+        text: 'PyPI',
+        link: 'https://pypi.org/pypi/randomuser'
+      },
+      {
+        text: 'Docs',
+        link: 'http://connordelacruz.com/python-randomuser/'
+      }
+    ],
+    body: 'A Python class for generating random user information using the randomuser.me API.',
+  },
+  {
+    title: "Python Lorem Ipsum Generator",
+    smallButtonsContent: [
+      {
+        text: 'GitHub',
+        link: 'https://github.com/connordelacruz/py-loremipsum'
+      },
+      {
+        text: 'PyPI',
+        link: 'https://pypi.org/pypi/py-loremipsum'
+      },
+      {
+        text: 'Docs',
+        link: 'http://connordelacruz.com/py-loremipsum/'
+      }
+    ],
+    body: 'A Python module for generating placeholder text using the loripsum.net API. Includes a command line tool to generate text from the terminal and copy it to the clipboard.',
+  },
+]
+
+// ================================================================================
+// Components
+// ================================================================================
+
+/**
+ * Image component for a project card.
+ *
+ * @param src
+ * @param alt
+ * @return {JSX.Element}
+ * @constructor
+ */
+const ProjectImage = ({src, alt}) => {
+  // TODO: size differently for horizontal/vertical
+  return (
+    <Box
+      sx={{
+        mb: 4,
+      }}
+    >
+      <img
+        src={src}
+        alt={alt}
+        className="project-image"
+      />
+    </Box>
+  )
+}
+
+/**
+ * Card component to showcase a project.
+ *
+ * @param title
+ * @param imgSrc
+ * @param bigButtonContent
+ * @param smallButtonsContent
+ * @param body
+ * @param children
+ * @return {JSX.Element}
+ * @constructor
+ */
+const ProjectCard = ({
+                       title,
+                       imgSrc = null,
+                       bigButtonContent = {text: '', link: ''},
+                       smallButtonsContent = [],
+                       body = null,
+                       children = null,
+                     }) => {
+  // Big button component (if applicable)
+  const bigButton = bigButtonContent.text ?
+    <Button
+      variant="contained"
+      color="primary"
+      size="large"
+      fullWidth
+      href={bigButtonContent.link}
+      target="_blank"
+    >
+      {bigButtonContent.text}
+    </Button>
+    : null
+  // Small buttons (if applicable)
+  const smallButtons = smallButtonsContent.map((smallButtonContent, i) =>
+    <Button
+      key={i}
+      variant="contained"
+      color="primary"
+      href={smallButtonContent.link}
+      target="_blank"
+    >
+      {smallButtonContent.text}
+    </Button>
+  )
+
+  return (
+    <Card>
+      <CardHeader
+        title={title}
+        slotProps={{
+          title: {
+            variant: 'h5',
+            color: 'primary',
+          }
+        }}
+      />
+      <CardContent>
+        {imgSrc && <ProjectImage src={imgSrc}/>}
+        <Typography variant="body1">
+          {children ? children : body}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {smallButtons}
+        {bigButton}
+      </CardActions>
+    </Card>
+  )
+}
+
+/**
+ * List of ProjectCard components.
+ *
+ * @param projectCardProps
+ * @return {JSX.Element}
+ * @constructor
+ */
+const ProjectCards = ({projectCardProps = projects}) => {
+  return (
+    <>
+      {
+        projectCardProps.map((cardProps, i) =>
+          <ProjectCard key={i} {...cardProps}/>
+        )
+      }
+    </>
+  )
+}
+
+/**
+ * Projects section component.
+ *
+ * @param color
+ * @return {JSX.Element}
+ * @constructor
+ */
+export const ProjectsSection = ({color}) => {
+  return (
+    <Box id="section-projects">
+      <SectionHeader color={color}>Projects</SectionHeader>
+      <Stack spacing={2}>
+        <ProjectCards/>
+        {/*View more button*/}
+        <Box id="github-link">
+          {/*TODO: big juicy CTA buttons*/}
+          <Button
+            variant="contained"
+            color={color}
+            startIcon={<GitHub/>}
+            size="large"
+            fullWidth
+            href="https://github.com/connordelacruz"
+            target="_blank"
+          >
+            View More on GitHub
+          </Button>
+        </Box>
+      </Stack>
+    </Box>
+  )
+}
